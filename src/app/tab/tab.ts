0c26669b212fbb5c9565171c9235f0651eb62898
lyusya <lyusya.cool@gmail.com>
2026-09-23 14:15:15 +0300

    feat: view child with template ref

--- src/app/tab/tab.ts
+++ src/app/tab/tab.ts
@@ -1,4 +1,4 @@
-import { Component, input, model } from '@angular/core';
+import { Component, ElementRef, input, model, ViewChild, viewChild } from '@angular/core';
 
 @Component({
   imports: [],
@@ -15,10 +15,49 @@ export class Tab {
   title = model.required<string>();
   active = input<boolean>(false);
   sizeTitle = model(20);
+  @ViewChild('tab') tabDecoratorView!: ElementRef<HTMLDivElement>;
+  tabSignalView = viewChild<ElementRef<HTMLDivElement>>('tab');
+
+  ngAfterViewInit() {
+    const tabDec = this.tabDecoratorView?.nativeElement;
+    tabDec.animate(
+      [
+        {
+          transform: 'translateY(0)',
+          opacity: 1,
+        },
+        {
+          transform: 'translateY(20px)',
+          opacity: 0,
+        },
+        {
+          transform: 'translateY(0)',
+          opacity: 1,
+        },
+      ],
+      {
+        duration: 1000,
+        easing: 'cubic-bezier(.2,.8,.2,1)',
+      }
+    )
+  }
 
   increaseSize() {
     this.sizeTitle.update((previousSize) => previousSize + 1);
     event?.stopPropagation();
+
+    const tabSig = this.tabDecoratorView?.nativeElement; 
+    tabSig.animate(
+      [
+        { transform: 'scale(1)' },
+        { transform: 'scale(1.05)' },
+        { transform: 'scale(1)' },
+      ],
+      {
+        duration: 200,
+        easing: 'ease-out',
+      }
+    );
   }
 
   decreaseSize() {
